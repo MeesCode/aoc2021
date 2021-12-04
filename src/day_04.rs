654a3c5e2f2bce970622e3a9185374cbf8d0e3fc
MeesCode <m.brinkhuis@student.tudelft.nl>
2021-12-04 14:24:06 +0100

    shorten

--- src/day_04.rs
+++ src/day_04.rs
@@ -3,7 +3,6 @@ use regex::Regex;
 #[derive(Clone, Copy)]
 struct Card {
     numbers: [[i32; 5]; 5],
-    won: bool
 }
 
 impl Card {
@@ -18,40 +17,28 @@ impl Card {
     }
 
     fn won(&self) -> bool {
-        if self.won { return true }
         for y in self.numbers {
-            if y.eq(&[-1; 5]) { /* self.won = true; */ return true }
-            
+            if y.eq(&[-1; 5]) { return true }
             'outer: for x in 0..5 {
                 for y in 0..5 {
                     if self.numbers[y][x] != -1 { continue 'outer; };
                 }
                 return true;
             }
-            
         }
         false
-    }
-
-    fn new() -> Card{
-        Card {
-            numbers: [[0; 5]; 5],
-            won: false
-        }
     }
 }
 
 pub fn run(){    
-    let mut input = include_str!("../data/day_04.txt").trim().lines();
-
-    let numbers: Vec<i32> = input.next().unwrap().split(",").map(|x| x.parse().unwrap()).collect();
+    let input = include_str!("../data/day_04.txt").trim();
+    let numbers: Vec<i32> = input.lines().next().unwrap().split(",").map(|x| x.parse().unwrap()).collect();
     let mut cards: Vec<Card> = Vec::new();
 
     let re = Regex::new(r"(\d+)\s*(\d+)\s*(\d+)\s*(\d+)\s*(\d+)\n").unwrap();
-
     let mut row = 0;
-    let mut cur_card = Card::new();
-    for cap in re.captures_iter(include_str!("../data/day_04.txt")) {
+    let mut cur_card = Card{ numbers: [[0; 5]; 5] };
+    for cap in re.captures_iter(input) {
         cur_card.numbers[row] = [
             cap[1].parse().unwrap(),
             cap[2].parse().unwrap(),
@@ -59,14 +46,11 @@ pub fn run(){
             cap[4].parse().unwrap(),
             cap[5].parse().unwrap(),
         ];
-
-        if row == 4 {
+        row += 1;
+        if row == 5 {
             cards.push(cur_card.clone());
             row = 0;
-            continue;
         }
-
-        row += 1;
     }
 
     let a = part_a(&numbers, &mut cards);
@@ -77,35 +61,23 @@ pub fn run(){
 
 fn part_a(numbers: &Vec<i32>, cards: &Vec<Card>) -> i32 {
     let mut cards = cards.clone();
-    let mut ret = 0;
-
     for n in numbers {
-        for c in &mut cards {
-            c.remove(*n);
-        }
-
+        for c in &mut cards { c.remove(*n);}
         if let Some(found) = cards.iter().find(|x| x.won()) {
-            ret = n * found.numbers.iter().fold(0, |acc, i| acc + i.iter().fold(0, |acc1, &j| acc1 + if j != -1 { j } else { 0 }));
-            break;
+            return n * found.numbers.iter().fold(0, |acc, i| acc + i.iter().fold(0, |acc1, &j| acc1 + if j != -1 { j } else { 0 }));
         }
     }
-
-    ret
+    0
 }
 
 fn part_b(numbers: &Vec<i32>, cards: &Vec<Card>) -> i32 {
     let mut cards = cards.clone();
-    let mut round = 0;
-
     for n in numbers {
-        for c in &mut cards {
-            c.remove(*n);
+        for c in &mut cards { c.remove(*n); }
+        if cards.len() == 1 && cards[0].won() { 
+            return n * cards[0].numbers.iter().fold(0, |acc, i| acc + i.iter().fold(0, |acc1, &j| acc1 + if j != -1 { j } else { 0 }));
         }
-
-        if cards.len() == 1 && cards[0].won() { round = *n; break; }
         cards = cards.iter().filter(|c| !c.won()).map(|x| *x).collect();
     }
-
-    let count = cards[0].numbers.iter().fold(0, |acc, i| acc + i.iter().fold(0, |acc1, &j| acc1 + if j != -1 { j } else { 0 }));
-    count * round
+    0
 }
