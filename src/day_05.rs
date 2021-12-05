17e75e069fbc33f1df85c570bea382ad32c9602f
Mees Brinkhuis <m.brinkhuis@student.tudelft.nl>
2021-12-05 16:22:20 +0100

    cleanup

--- src/day_05.rs
+++ src/day_05.rs
@@ -4,16 +4,12 @@ use std::cmp;
 
 pub fn run(){    
     let input = include_str!("../data/day_05.txt").trim();
-
     let re = Regex::new(r"(\d+),(\d+) -> (\d+),(\d+)").unwrap();
-
     let mut lines: Vec<((i32, i32), (i32, i32))> = Vec::new();
     
     for cap in re.captures_iter(input) {
         lines.push(((cap[1].parse().unwrap(), cap[2].parse().unwrap()), (cap[3].parse().unwrap(), cap[4].parse().unwrap())));
     }
-
-    // println!("{:?}", lines);
 
     let a = part_a(&lines);
     println!("Part A result: {}", a);
@@ -79,56 +75,42 @@ fn part_b(lines: &Vec<((i32, i32), (i32, i32))>) -> i32{
 
         // diagonal
         else {
-            let mut x;
+
+            let dx;
+            let start;
+            let endy;
+
             if i.0.0 < i.1.0 {
-
                 if i.0.1 < i.1.1 {
-                    x = i.0.0;
-                    for y in i.0.1..i.1.1+1 {
-                        if let Some(v) = values.get_mut(&(x, y)) {
-                            *v += 1;
-                        } else {
-                            values.insert((x, y), 1);
-                        }
-                        x += 1;
-                    } 
+                    dx = 1;
+                    start = (i.0.0, i.0.1);
+                    endy = i.1.1;
                 } else {
-                    x = i.1.0;
-                    for y in i.1.1..i.0.1+1 {
-                        if let Some(v) = values.get_mut(&(x, y)) {
-                            *v += 1;
-                        } else {
-                            values.insert((x, y), 1);
-                        }
-                        x -= 1;
-                    } 
+                    dx = -1;
+                    start = (i.1.0, i.1.1);
+                    endy = i.0.1;
                 }
-                
             } else {
-
                 if i.0.1 < i.1.1 {
-                    x = i.0.0;
-                    for y in i.0.1..i.1.1+1 {
-                        if let Some(v) = values.get_mut(&(x, y)) {
-                            *v += 1;
-                        } else {
-                            values.insert((x, y), 1);
-                        }
-                        x -= 1;
-                    } 
+                    dx = -1;
+                    start = (i.0.0, i.0.1);
+                    endy = i.1.1;
                 } else {
-                    x = i.1.0;
-                    for y in i.1.1..i.0.1+1 {
-                        if let Some(v) = values.get_mut(&(x, y)) {
-                            *v += 1;
-                        } else {
-                            values.insert((x, y), 1);
-                        }
-                        x += 1;
-                    } 
+                    dx = 1;
+                    start = (i.1.0, i.1.1);
+                    endy = i.0.1;
                 }
-                
-            };
+            }
+
+            let mut x = start.0;
+            for y in start.1..endy+1 {
+                if let Some(v) = values.get_mut(&(x, y)) {
+                    *v += 1;
+                } else {
+                    values.insert((x, y), 1);
+                }
+                x += dx;
+            }
             
         }
     }
