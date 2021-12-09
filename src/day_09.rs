28d0fae509cf7593b8d86e4658d1a3736f4317cc
MeesCode <m.brinkhuis@student.tudelft.nl>
2021-12-09 09:34:42 +0100

    cleanup

--- src/day_09.rs
+++ src/day_09.rs
@@ -5,9 +5,7 @@ pub fn main(){
     
     let mut grid: Vec<Vec<i32>> = Vec::new();
     for i in input {
-        grid.push(
-            i.chars().map(|x| x.to_digit(10).unwrap() as i32).collect()
-        )
+        grid.push( i.chars().map(|x| x.to_digit(10).unwrap() as i32).collect() )
     }
 
     let a = part_a(&grid);
@@ -16,90 +14,47 @@ pub fn main(){
     println!("Part B result: {}", b);
 }
 
-fn part_a(grid: &Vec<Vec<i32>>) -> i32 {
-    let mut result = 0;
-
-    for y in 0 as i32..grid.len() as i32 {
-        'outer: for x in 0 as i32..grid[0].len() as i32 {
-
-            for dy in y as i32-1..y as i32+2 {
-                for dx in x as i32-1..x as i32+2 {
-                    if dx == x && dy == y { continue; }
-                    if dx == -1 || dx >= grid[0].len() as i32 || dy == -1 || dy >= grid.len() as i32 { continue; }
-                    if grid[dy as usize][dx as usize] < grid[y as usize][x as usize] { continue 'outer; } 
-                }
-            }
-
-            result += grid[y as usize][x as usize] + 1;
-        }
-    }
-
-    result
-}
-
-fn part_b(grid: &Vec<Vec<i32>>) -> i32 {
-    let mut result = 1;
+fn get_lowest(grid: &Vec<Vec<i32>>) -> Vec<(usize, usize)> {
     let mut lows = Vec::new();
-    let mut sizes = Vec::new();
-
-    for y in 0 as i32..grid.len() as i32 {
-        'outer: for x in 0 as i32..grid[0].len() as i32 {
-
-            for dy in y as i32-1..y as i32+2 {
-                for dx in x as i32-1..x as i32+2 {
-                    if dx == x && dy == y { continue; }
-                    if dx == -1 || dx >= grid[0].len() as i32 || dy == -1 || dy >= grid.len() as i32 { continue; }
-                    if grid[dy as usize][dx as usize] < grid[y as usize][x as usize] { continue 'outer; } 
-                }
+    for y in 0..grid.len() {
+        'outer: for x in 0..grid[0].len() {
+            for (dx, dy) in [(x, y-1), (x-1, y), (x+1, y), (x, y+1), (x-1, y-1), (x-1, y+1), (x+1, y-1), (x+1, y+1)] {
+                if dx >= grid[0].len() || dy >= grid.len() { continue; }
+                if grid[dy][dx] < grid[y][x] { continue 'outer; } 
             }
-
             lows.push((x, y));
         }
     }
+    lows
+}
 
-    for (x, y) in lows {
+fn part_a(grid: &Vec<Vec<i32>>) -> i32 {
+    get_lowest(grid).iter().fold(0, |a, &(x, y)| a + grid[y][x] + 1)
+}
 
+fn part_b(grid: &Vec<Vec<i32>>) -> i32 {
+    let mut sizes = Vec::new();
+    for (x, y) in get_lowest(grid) {
         let mut visited = HashSet::new();
         visited.insert((x,y));
-        let mut current = vec![(x,y)];
+        let mut current: Vec<(usize, usize)> = vec![(x,y)];
 
         while !current.is_empty() {
-
             let mut new_current = Vec::new();
-
-            for (cx, cy) in &current {
-
-                for dy in *cy as i32-1..*cy as i32+2 {
-                    for dx in *cx as i32-1..*cx as i32+2 {
-
-                        if dx == *cx && dy == *cy { continue; }
-                        if dx == -1 || dx >= grid[0].len() as i32 || dy == -1 || dy >= grid.len() as i32 { continue; }
-                        if dx != *cx && dy != *cy { continue; }
-
-                        // println!("({}, {})", dx, dy);
-
-                        if grid[dy as usize][dx as usize] != 9 && !visited.contains(&(dx, dy)) {
-                            new_current.push((dx, dy));
-                            visited.insert((dx, dy));
-                        }
+            for (cx, cy) in current {
+                for (dx, dy) in [(cx, cy-1), (cx-1, cy), (cx+1, cy), (cx, cy+1)] {
+                    if dx >= grid[0].len() || dy >= grid.len() { continue; }
+                    if grid[dy][dx] != 9 && !visited.contains(&(dx, dy)) {
+                        new_current.push((dx, dy));
+                        visited.insert((dx, dy));
                     }
                 }
-
             }
-
             current = new_current.clone();
         }
-
         sizes.push(visited.len() as i32);
-
     }
 
     sizes.sort();
-
-    for i in sizes.len()-3..sizes.len() {
-        result *= sizes[i];
-    }
-
-    result
+    sizes.pop().unwrap() * sizes.pop().unwrap() * sizes.pop().unwrap()
 }
-
