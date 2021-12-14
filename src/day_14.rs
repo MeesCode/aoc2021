c75221b81d2b9f393804c29d29893877ab02ed90
Meess Brinkhuis <m.brinkhuis@student.tudelft.nl>
2021-12-14 12:38:02 +0100

    part 2

--- src/day_14.rs
+++ src/day_14.rs
@@ -13,46 +13,42 @@ pub fn main(){
         insertions.insert((parts[0].chars().nth(0).unwrap(), parts[0].chars().nth(1).unwrap()), parts[1].chars().nth(0).unwrap());
     }
 
-    let a = part_a(&template, &insertions);
+    let a = part_ab(&template, &insertions, 10);
     println!("Part 1: {}", a);
-    // let b = part_b(&input);
-    // println!("Part 2: {}", b);
+    let b = part_ab(&template, &insertions, 40);
+    println!("Part 2: {}", b);
 }
 
-fn part_a(template: &Vec<char>, insertions: &HashMap<(char, char), char>) -> i32 {
+fn part_ab(template: &Vec<char>, insertions: &HashMap<(char, char), char>, steps: usize) -> i64 {
+    let mut letters: HashMap<char, i64> = HashMap::new();
+    for i in 'A'..'Z' { letters.insert(i, 0); }
+    for i in template { *letters.get_mut(i).unwrap() += 1; }
 
-    let mut template = template.clone();
+    let mut combos: HashMap<(char, char), i64> = HashMap::new();
+    for i in 'A'..'Z' { for j in 'A'..'Z' { combos.insert((i, j), 0); }}
+    for i in 0..template.len()-1 {
+        *combos.get_mut(&(template[i], template[i+1])).unwrap() += 1;
+    };
 
-    for _step in 1..41 {
-        
-        let mut i = 0;
-        while i < template.len()-1 {
+    for _step in 1..steps+1 {
+        let mut new_combos = combos.clone();
 
-            if let Some(c) = insertions.get(&(template[i], template[i+1])) {
-                template.insert(i+1, *c);
-                i += 1;
-            }
-
-            i += 1;
+        for (combination, letter) in insertions {
+            let amount = combos.get(&combination).unwrap();
+            *new_combos.get_mut(&combination).unwrap() -= amount;
+            *new_combos.get_mut(&(combination.0, *letter)).unwrap() += amount;
+            *new_combos.get_mut(&(*letter, combination.1)).unwrap() += amount;
+            *letters.get_mut(&letter).unwrap() += amount;
         }
 
-        println!("step {}", _step);
+        combos = new_combos;
     }
 
-    let mut count: HashMap<char, i32> = HashMap::new();
-    for i in template {
-        if let Some(c) = count.get_mut(&i) {
-            *c += 1;
-        } else {
-            count.insert(i, 1);
-        }
-    }
-
-    let mut min = i32::max_value();
+    let mut min = i64::max_value();
     let mut max = 0;
-    for (_, i) in count {
+    for (_, i) in letters {
         if i > max { max = i; }
-        if i < min { min = i; }
+        if i > 0 && i < min { min = i; }
     }
 
     max - min
