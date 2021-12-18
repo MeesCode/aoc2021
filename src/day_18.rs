0163eaf3aaed52de08dd1e065ca8d096fbc8ef89
Meess Brinkhuis <m.brinkhuis@student.tudelft.nl>
2021-12-18 21:21:55 +0100

    cleanup

--- src/day_18.rs
+++ src/day_18.rs
@@ -1,6 +1,9 @@
 
 use std::mem;
+use itertools::Itertools;
+use std::cmp;
 
+#[derive(Clone)]
 enum Value {
     Pair((Box<Value>, Box<Value>)),
     Literal(i32)
@@ -9,56 +12,42 @@ enum Value {
 use Value::{Pair, Literal};
 
 pub fn main(){    
-    let mut input = include_str!("../data/day_18.txt").trim().lines();
-    let mut tree = parse(input.next().unwrap(), 0).0;
-
-    for i in input {
-        tree = Pair((Box::new(tree), Box::new(parse(i, 0).0)));
-        reduce(&mut tree);
+    let input: Vec<Value> = include_str!("../data/day_18.txt").trim().lines().map(|x| parse(x, 0).0).collect();
+    
+    let mut tree = input[0].clone();
+    for i in 1..input.len() {
+        tree = reduce(&Pair((Box::new(tree), Box::new(input[i].clone()))));
     }
-
     println!("part 1: {}", magnitude(&tree));
 
-    let all_input: Vec<&str> = include_str!("../data/day_18.txt").trim().lines().collect();
-    let mut max = 0;
-
-    for i in &all_input {
-        for j in &all_input {
-            if i.eq(j) { continue }
-
-            tree = Pair((Box::new(parse(i, 0).0), Box::new(parse(j, 0).0)));
-            let val = reduce(&mut tree);
-            if val > max { max = val; }
-        }
-    }
-
-    println!("part 2: {}", max);
+    let b = (0..input.len()).permutations(2).fold(0, |a, x| cmp::max(a, if x[0] == x[1] {0} else { magnitude(&reduce(&Pair((Box::new(input[x[0]].clone()), Box::new(input[x[1]].clone()))))) } ));
+    println!("part 2: {}", b);
 }
 
-fn reduce(tree: &mut Value) -> i32{
+fn reduce(tree: &Value) -> Value {
+    let mut tree = tree.clone();
     loop {
-        if !explode(tree) && !split(tree) {break;}
+        if !explode(&mut tree) && !split(&mut tree) { break; }
     }
-    magnitude(tree)
+    tree
 }
 
 fn split(tree: &mut Value) -> bool{
-    if let Pair((left, right)) = tree {
-        if split(left) { return true }
-        return split(right);
-    }
-
-    if let Literal(val) = tree {
-        if *val >= 10 {
-            let l = *val/2;
-            let r = *val/2 + if *val % 2 != 0 {1} else {0};
-
-            let _ = mem::replace(tree, Pair( (Box::new(Literal(l)), Box::new(Literal(r)))));
-            return true;
+    match tree {
+        Pair((left, right)) => {
+            if split(left) { return true; }
+            split(right)
+        },
+        Literal(val) => {
+            if *val >= 10 {
+                let l = *val/2;
+                let r = *val/2 + if *val % 2 != 0 {1} else {0};
+                let _ = mem::replace(tree, Pair((Box::new(Literal(l)), Box::new(Literal(r)))));
+                return true;
+            }
+            false
         }
     }
-
-    false
 }
 
 fn explode(tree: &mut Value) -> bool {
@@ -69,59 +58,55 @@ fn explode(tree: &mut Value) -> bool {
 }
 
 fn fill(tree: &mut Value, index: i32, target: i32, l: i32, r: i32) -> i32 {
-    if let Pair((left, right)) = tree {
-        let i = fill(left, index, target, l, r);
-        let i = fill(right, i, target, l, r);
-        return i;
-    }
-
-    if let Literal(val) = tree {
-        if index == target - 1 {
-            *val += l;
+    match tree {
+        Pair((left, right)) => {
+            let i = fill(left, index, target, l, r);
+            fill(right, i, target, l, r)
+        },
+        Literal(val) => {
+            if index == target - 1 {
+                *val += l;
+            }
+            if index == target + 1 {
+                *val += r;
+            }
+            index+1
         }
-        if index == target + 1 {
-            *val += r;
-        }
-        return index+1;
     }
-
-    panic!("unhandeled recusive return");
 }
 
 fn explode_step(tree: &mut Value, depth: i32, index: i32) -> (i32, bool, i32, i32) {
-    
-    if let Pair((left, right)) = tree {
-        if depth == 4 {
+    match tree {
+        Pair((left, right)) => {
+            if depth == 4 {
 
-            let l = match **left {
-                Literal(x) => x,
-                _ => 0
-            };
-            let r = match **right {
-                Literal(x) => x,
-                _ => 0
-            };
+                let l = match **left {
+                    Literal(x) => x,
+                    _ => 0
+                };
+                let r = match **right {
+                    Literal(x) => x,
+                    _ => 0
+                };
 
-            let _ = mem::replace(tree, Literal(0));
+                let _ = mem::replace(tree, Literal(0));
 
-            return (index, true, l, r);
-        } else {
-            let (l, foundl, le, ri) = explode_step(left, depth + 1, index);
-            if foundl { return (l, true, le, ri); }
-            let (r, foundr, le, ri) = explode_step(right, depth + 1, l);
-            if foundr {
-                return (r, true, le, ri);
+                (index, true, l, r)
             } else {
-                return (r, false, le, ri);
+                let (l, foundl, le, ri) = explode_step(left, depth + 1, index);
+                if foundl { return (l, true, le, ri); }
+                let (r, foundr, le, ri) = explode_step(right, depth + 1, l);
+                if foundr {
+                    (r, true, le, ri)
+                } else {
+                    (r, false, le, ri)
+                }
             }
+        },
+        Literal(_) => {
+            (index+1, false, 0, 0)
         }
     }
-
-    if let Literal(_) = tree {
-        return (index+1, false, 0, 0);
-    }
-
-    panic!("unhandeled recusive return");
 }
 
 fn magnitude(tree: &Value) -> i32 {
